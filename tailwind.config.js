38bc2ba2461c7b9a70a00118a7f5fb389a0e344e
robin <robin@baldwin.be>
2021-12-26 12:57:05 +0100

    Init commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      width: {
+        '12px': '12px',
+        '400px': '400px',
+      },
+      height: {
+        '12px': '12px'
+      },
+      colors: {
+        'custom-zink-100': '#FFFEFF',
+        'custom-zink-200': '#424145',
+        'custom-zink-700': '#24232A',
+        'custom-zink-800': '#1E1D21',
+        'custom-zink-900': '#18171F'
+      }
+    }
+  },
+  plugins: [],
+}
