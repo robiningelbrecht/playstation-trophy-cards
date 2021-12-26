7ac3290df0a1793371ec7a66f488e824435db16a
robin <robin@baldwin.be>
2021-12-26 14:49:30 +0100

    Backside of card

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,10 @@ module.exports = {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
       },
-      width: {
-        '12px': '12px',
-        '400px': '400px',
-      },
-      height: {
-        '12px': '12px'
-      },
       colors: {
         'custom-zink-100': '#FFFEFF',
         'custom-zink-200': '#424145',
+        'custom-zink-600': '#2f2e35',
         'custom-zink-700': '#24232A',
         'custom-zink-800': '#1E1D21',
         'custom-zink-900': '#18171F'
